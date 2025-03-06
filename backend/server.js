41f76a074a30445ed35fad0a26f1b98d98c78e57
sashi-does <sashiontop@gmail.com>
2025-03-06 15:03:55 +0530

    fix: vercel.json

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import userPreferencesRouter from './routes/userPreferencesRoute.js'
 import watchLaterRouter from './routes/watchLaterRoute.js'
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 mongoDB();
 
